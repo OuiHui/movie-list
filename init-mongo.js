98aae79403be9430ef5924fdb1c67ae04ab15bce
Nguyen, Huy <hnguyen499@gatech.edu>
2025-09-26 15:47:42 -0400

    Fix grid size issue

--- init-mongo.js
+++ init-mongo.js
@@ -1,10 +1,5 @@
-// MongoDB initialization script
-// This script runs when the MongoDB container starts for the first time
-
-// Switch to movieapp database
 db = db.getSiblingDB('movieapp');
 
-// Create a user for the application
 db.createUser({
   user: 'movieapp',
   pwd: 'movieapppassword',
@@ -16,18 +11,15 @@ db.createUser({
   ]
 });
 
-// Create initial collections with indexes
+
 db.createCollection('lists');
 db.createCollection('movies');
-
-// Create indexes for better performance
 db.lists.createIndex({ "name": 1 });
 db.lists.createIndex({ "isDefault": 1 });
 db.movies.createIndex({ "listId": 1 });
 db.movies.createIndex({ "rank": 1 });
 db.movies.createIndex({ "title": 1 });
 
-// Insert a default list
 db.lists.insertOne({
   name: "My Movies",
   description: "Default movie list",
